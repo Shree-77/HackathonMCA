29f2ebd087d852f81ea7c832229f4af00c611b80
Shree-77 <shreedhoni7@gmail.com>
2023-05-20 16:40:23 +0530

    Images need to be updated

--- src/Pages/ClickableImageMatrix.java
+++ src/Pages/ClickableImageMatrix.java
@@ -1,6 +1,5 @@
 package Pages;
 
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -13,9 +12,8 @@ public class ClickableImageMatrix extends JFrame implements ActionListener {
     private List<ImageIcon> imageList;
     private List<JButton> imageButtons;
     private JButton submitButton;
-    Login l;
-    
-    private ArrayList<Integer> selectedIndices;
+    private CardLayout cardLayout;
+    private JPanel cardPanel;
 
     public ClickableImageMatrix() {
         setTitle("Clickable Image Matrix");
@@ -23,23 +21,26 @@ public class ClickableImageMatrix extends JFrame implements ActionListener {
         setSize(400, 400);
         setLocationRelativeTo(null); // Center the frame on the screen
 
+        // Create the card layout and panel
+        cardLayout = new CardLayout();
+        cardPanel = new JPanel(cardLayout);
+
         JPanel panel = new JPanel(new GridLayout(3, 3));
 
         // Create a list of image paths
         List<String> imagePaths = new ArrayList<>();
-        imagePaths.add("C:\\Users\\ELCOT\\Desktop\\HackathonMCA\\src\\img\\image_part_001.png");
-        imagePaths.add("C:\\Users\\ELCOT\\Desktop\\HackathonMCA\\image_part_002.png");
-        imagePaths.add("C:\\Users\\ELCOT\\Desktop\\HackathonMCA\\image_part_003.png");
-        imagePaths.add("C:\\Users\\ELCOT\\Desktop\\HackathonMCA\\image_part_004.png");
-        imagePaths.add("C:\\Users\\ELCOT\\Desktop\\HackathonMCA\\image_part_005.png");
-        imagePaths.add("C:\\Users\\ELCOT\\Desktop\\HackathonMCA\\image_part_006.png");
-        imagePaths.add("C:\\Users\\ELCOT\\Desktop\\HackathonMCA\\image_part_007.png");
-        imagePaths.add("C:\\Users\\ELCOT\\Desktop\\HackathonMCA\\image_part_008.png");
-        imagePaths.add("C:\\Users\\ELCOT\\Desktop\\HackathonMCA\\image_part_009.png");
+        imagePaths.add("/Users/shree/Documents/JavaPrograms/HackathonMCA/src/img/image_part_001.pngs");
+        imagePaths.add("img/image_part_002.png");
+        imagePaths.add("img/image_part_003.png");
+        imagePaths.add("img/image_part_004.png");
+        imagePaths.add("img/image_part_005.png");
+        imagePaths.add("img/image_part_006.png");
+        imagePaths.add("img/image_part_007.png");
+        imagePaths.add("img/image_part_008.png");
+        imagePaths.add("img/image_part_009.png");
 
         imageList = new ArrayList<>();
         imageButtons = new ArrayList<>();
-        selectedIndices = new ArrayList<>();
 
         // Create and add buttons with images to the panel
         for (String imagePath : imagePaths) {
@@ -59,7 +60,17 @@ public class ClickableImageMatrix extends JFrame implements ActionListener {
         mainPanel.add(panel, BorderLayout.CENTER);
         mainPanel.add(submitButton, BorderLayout.SOUTH);
 
-        getContentPane().add(mainPanel);
+        cardPanel.add(mainPanel, "matrix");
+        getContentPane().add(cardPanel);
+
+        // Add your new homepage panel here (replace the placeholder)
+        JPanel newHomePagePanel = new JPanel();
+        JLabel newHomePageLabel = new JLabel("Welcome to the new homepage!");
+        newHomePagePanel.add(newHomePageLabel);
+
+
+
+        cardPanel.add(newHomePagePanel, "newHomepage");
     }
 
     public void actionPerformed(ActionEvent e) {
@@ -70,28 +81,35 @@ public class ClickableImageMatrix extends JFrame implements ActionListener {
             int selectedIndex = imageButtons.indexOf(clickedButton);
 
             // Check if the image is already selected
-            if (!selectedIndices.contains(selectedIndex)) {
-                selectedIndices.add(selectedIndex);
-                clickedButton.setEnabled(false); // Disable the button after selecting
-
-                System.out.println("Button " + selectedIndex + " clicked.");
-
-                // Check if three images are selected
-                if (selectedIndices.size() == 3) {
-                	
-                	selectedIndices.clear();
-                    submitButton.setEnabled(true); // Enable the submit button
-                }
+            if (!clickedButton.isEnabled()) {
+                return; // Ignore clicks on disabled buttons
             }
+
+            // Check if three images are already selected
+            if (getSelectedImageCount() >= 3) {
+                return; // Ignore clicks when three images are already selected
+            }
+
+            clickedButton.setEnabled(false); // Disable the button after selecting
+            System.out.println("Button " + selectedIndex + " clicked.");
         } else if (source == submitButton) {
             // Handle the action for the submit button
-        	Login l=new Login();
-        	l.setLocationRelativeTo(null);
-            l.setTitle("Login");
-            l.setVisible(true);
-            System.out.println("Submit button clicked.");
-            // Navigate to the next page or perform the desired action
+            if (getSelectedImageCount() == 3) {
+                System.out.println("Submit button clicked.");
+                JOptionPane.showMessageDialog(this, "Password correct!"); // Show pop-up message
+                cardLayout.show(cardPanel, "newHomepage"); // Show the new homepage panel
+            }
         }
+    }
+
+    private int getSelectedImageCount() {
+        int count = 0;
+        for (JButton button : imageButtons) {
+            if (!button.isEnabled()) {
+                count++;
+            }
+        }
+        return count;
     }
 
     public static void main(String[] args) {
@@ -99,4 +117,3 @@ public class ClickableImageMatrix extends JFrame implements ActionListener {
         example.setVisible(true);
     }
 }
-
